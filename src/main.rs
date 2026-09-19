9986bae207b4b5659f83d3f64f61edb0f5ab07ad
Samay Kapadia <1297369+sa-@users.noreply.github.com>
2026-09-19 02:36:28 +0200

    full screen, keyboard nav

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use std::process::Command;
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
+    ui.on_launch_app(move |app_name| {
+        let script = format!("display dialog \"{}\"", app_name.replace('"', "\\\""));
+
+        if let Err(error) = Command::new("osascript").args(["-e", &script]).spawn() {
+            eprintln!("Could not launch {app_name}: {error}");
+        }
+    });
+
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+    ui.on_quit(move || {
+        ui_handle.unwrap().hide().expect("Could not close Farbig Launcher");
     });
 
     ui.run()?;
