fe33b6bdd20982c88bfca59cd69632e5b1a9932b
Samay Kapadia <1297369+sa-@users.noreply.github.com>
2026-09-19 12:13:47 +0200

    load mac apps

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,42 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use std::fs;
 use std::process::Command;
 
 use slint::{ModelRc, SharedString, VecModel};
 
 slint::include_modules!();
 
-fn apps() -> Vec<App> {
-    vec![
-        App {
-            name: SharedString::from("App 1"),
-        },
-        App {
-            name: SharedString::from("App 2"),
-        },
-    ]
+fn apps() -> Result<Vec<App>, std::io::Error> {
+    let mut bundles = fs::read_dir("/Applications")?
+        .filter_map(Result::ok)
+        .filter_map(|entry| {
+            let path = entry.path();
+            (path.extension().is_some_and(|extension| extension == "app")).then_some(path)
+        })
+        .filter_map(|path| {
+            let name = path.file_stem()?.to_string_lossy().into_owned();
+            let path = path.to_string_lossy().into_owned();
+
+            Some(App {
+                name: SharedString::from(name),
+                path: SharedString::from(path),
+            })
+        })
+        .collect::<Vec<_>>();
+
+    bundles.sort_by(|left, right| left.name.cmp(&right.name));
+    Ok(bundles)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
-    ui.set_apps(ModelRc::new(VecModel::from(apps())));
+    ui.set_apps(ModelRc::new(VecModel::from(apps()?)));
 
-    ui.on_launch_app(move |app_name| {
-        let script = format!("display dialog \"{}\"", app_name.replace('"', "\\\""));
-
-        if let Err(error) = Command::new("osascript").args(["-e", &script]).spawn() {
-            eprintln!("Could not launch {app_name}: {error}");
+    ui.on_launch_app(move |app_path| {
+        if let Err(error) = Command::new("open").arg(app_path.as_str()).spawn() {
+            eprintln!("Could not launch {app_path}: {error}");
         }
     });
 
